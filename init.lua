7ccb0c3e6476a719ea29cba947f9a8bb38b84c4c
Yiu Man Ho <yiufamily.hh@gmail.com>
2020-09-07 19:00:43 +0800

    Disallow some block

--- init.lua
+++ init.lua
@@ -1,3 +1,27 @@
+local deny_list = {}
+deny_list["ignore"] = true
+deny_list["air"] = true
+deny_list["default:water_flowing"] = true
+deny_list["default:river_water_flowing"] = true
+deny_list["default:lava_flowing"] = true
+deny_list["default:cloud"] = true
+deny_list["mesecons_pistons:piston_normal_on"] = true
+deny_list["mesecons_pistons:piston_pusher_normal"] = true
+deny_list["mesecons_pistons:piston_sticky_on"] = true
+deny_list["mesecons_pistons:piston_pusher_sticky"] = true
+deny_list["default:chest_locked_open"] = true
+deny_list["default:chest_open"] = true
+
+minetest.register_on_mods_loaded(function()
+	for p,v in pairs(minetest.registered_nodes)
+	do
+		if minetest.registered_nodes[p.."_active"] then
+			deny_list[p.."_active"] = true
+		end
+	end
+end)
+
+
 minetest.register_craftitem("getter:getter",{
 	description = "Getter (Right click to get thing that you pointing)",
 	groups = {tools = 1},
@@ -10,6 +34,12 @@ minetest.register_craftitem("getter:getter",{
 		end
 		local pos = minetest.get_pointed_thing_position(pointed_thing, false)
 		local node = minetest.get_node(pos)
+		if not(minetest.registered_items[node.name]) then
+			return
+		end
+		if deny_list[node.name] then
+			return
+		end
 		if minetest.registered_items[node.name].groups and minetest.registered_items[node.name].groups.unbreakable then
 			return
 		end
